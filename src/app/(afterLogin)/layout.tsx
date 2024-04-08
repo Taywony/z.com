cb3531682e3d144a50b490250dbe74b3c950fd23
taewon kim <taewon2659@gmail.com>
2024-04-08 23:05:45 +0900

    leftSection Framing

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -15,31 +15,60 @@ export default function AfterLoginLayout({
   return (
     <div className={style.container}>
       <div className={style.leftSectionWrapper}>
-        <Link href="/home" className={style.leftSectionLogoBox}>
-          <Image src={Zlogo} alt="Zlogo" className={style.leftSectionLogo} />
-        </Link>
-        <Link href="/home" className={style.leftMenuBox}>
-          <div className={style.leftMenu}>
-            <Image src={Home} alt="home" className={style.leftMenuIcon} />
-            <div className={style.leftMenuTitle}>Home</div>
+        <div className={style.menuTabSection}>
+          <div className={style.logoBox}>
+            <Link href="/home" className={style.logoLink}>
+              <Image src={Zlogo} alt="Zlogo" className={style.logoImg} />
+            </Link>
           </div>
-        </Link>
-        <Link href="/explore" className={style.leftMenuBox}>
-          <div className={style.leftMenu}>
-            <Image src={Exlpore} alt="exlpore" className={style.leftMenuIcon} />
-            <div className={style.leftMenuTitle}>Explore</div>
+          <div className={style.menuBox}>
+            <Link href="/home" className={style.menuLink}>
+              <div className={style.menuLinkBox}>
+                <Image src={Home} alt="home" className={style.menuIcon} />
+                <p className={style.menuTitle}>Home</p>
+              </div>
+            </Link>
+            <Link href="/explore" className={style.menuLink}>
+              <div className={style.menuLinkBox}>
+                <Image src={Exlpore} alt="exlpore" className={style.menuIcon} />
+                <p className={style.menuTitle}>Explore</p>
+              </div>
+            </Link>
+            <Link href="notifications" className={style.menuLink}>
+              <div className={style.menuLinkBox}>
+                <Image
+                  src={Notifications}
+                  alt="noti"
+                  className={style.menuIcon}
+                />
+                <p className={style.menuTitle}>Notifications</p>
+              </div>
+            </Link>
           </div>
-        </Link>
-        <Link href="notifications" className={style.leftMenuBox}>
-          <div className={style.leftMenu}>
-            <Image
-              src={Notifications}
-              alt="noti"
-              className={style.leftMenuIcon}
-            />
-            <div className={style.leftMenuTitle}>Notifications</div>
+          <div className={style.postBtnBox}>
+            <Link href="/post" className={style.postBtn}>
+              <p className={style.postBtnText}>Post</p>
+            </Link>
           </div>
-        </Link>
+        </div>
+        <div className={style.userTabSection}>
+          <button className={style.userBtn}>
+            <div className={style.userImgBox}>
+              <Image src={Home} alt="userImage" className={style.userImg} />
+            </div>
+            <div className={style.userInfoBox}>
+              <div className={style.userNameBox}>
+                <p className={style.userName}>김태원</p>
+              </div>
+              <div className={style.userEmailBox}>
+                <p className={style.userEmail}>@taewon2659</p>
+              </div>
+            </div>
+            <div className={style.userMoreBox}>
+              <Image src={Home} alt="more" className={style.userMore} />
+            </div>
+          </button>
+        </div>
       </div>
       <div className={style.rightSectionWrapper}>오른쪽</div>
     </div>
