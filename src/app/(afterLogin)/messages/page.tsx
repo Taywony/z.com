2babb71cde20c65489c804d3473e91d1a32a7fd0
taewon kim <taewon2659@gmail.com>
2024-06-09 23:52:27 +0900

    Messages page Navigator Send Message Users layout

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -2,6 +2,12 @@ import NewMessageButton from "./_component/NewMessageButton";
 import SettingButton from "./_component/SettingButton";
 import style from "./message.module.css";
 
+// function MessagesUser () {
+//   return (
+
+//   )
+// }
+
 const Message = () => {
   return (
     <div className={style.container}>
@@ -24,13 +30,51 @@ const Message = () => {
               </div>
             </div>
             {/* 유저 네비게이터부분 */}
-            <div className={style.navigator}>{/* 네비게이터 유저들 */}</div>
+            <div className={style.naviMain}>
+              <div className={style.userMainBox}>
+                <div className={style.userImageBox}>
+                  <img
+                    src="https://picsum.photos/50/50"
+                    className={style.userImage}
+                  />
+                </div>
+                <div className={style.user}>
+                  <div className={style.user}></div>
+                </div>
+              </div>
+              <div className={style.userMainBox}>
+                <div className={style.userImageBox}>
+                  <img
+                    src="https://picsum.photos/50/50"
+                    className={style.userImage}
+                  />
+                </div>
+                <div className={style.userInfoBox}>
+                  <div className={style.messageInfoZone}>
+                    <div className={style.messageInfo}>
+                      <div className={style.userInfo}>
+                        <div className={style.userName}>김태원</div>
+                        <div className={style.userEmail}>@taewon2659</div>
+                      </div>
+                      <div className={style.middleDot}> · </div>
+                      <div className={style.sendDate}>Jun 9</div>
+                    </div>
+                    <div className={style.messagePreview}>
+                      <span className={style.messagePrivewText}>
+                        미리보기 미리보기
+                      </span>
+                    </div>
+                  </div>
+                  <div className={style.messageSettingButton}></div>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
-        {/* 오른쪽 섹션 */}
+        {/* 오른쪽 섹션 
         <section className={style.detailSection}>
           <div className={style.detailBox}>
-            {/* 디테일 헤더 */}
+            {/* 디테일 헤더 
             <div className={style.detailHeader}>
               <div className={style.detailHeaderBox}>
                 <div className={style.detailHeaderIconZone}></div>
@@ -39,12 +83,12 @@ const Message = () => {
                 </div>
               </div>
             </div>
-            {/* 디테일 리스트 */}
+            {/* 디테일 리스트 
             <div className={style.detailInfo}>
-              <div className={style.detailInfoBox}>{/* 디테일 유저들 */}</div>
+              <div className={style.detailInfoBox}>{/* 디테일 유저들 </div>
             </div>
           </div>
-        </section>
+        </section>*/}
       </div>
     </div>
   );
