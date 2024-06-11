774bbb893972ac8d44114d47d0989745a1f0e36b
taewon kim <taewon2659@gmail.com>
2024-06-11 17:10:05 +0900

    Modify: Message Room ChatingBox CSS

--- src/app/(afterLogin)/messages/[room]/page.tsx
+++ src/app/(afterLogin)/messages/[room]/page.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import Link from "next/link";
 import style from "./chatRoom.module.css";
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
+import "dayjs/locale/en";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { useRouter } from "next/navigation";
 
-dayjs.locale("ko");
+dayjs.locale("en");
 dayjs.extend(relativeTime);
 
 const ChatRoom = () => {
+  const router = useRouter();
+
   const userInfo = {
     id: "Foden47",
     name: "Foden",
@@ -25,10 +31,26 @@ const ChatRoom = () => {
     {
       RoomId: "567",
       id: "taewon2659",
-      content: "축하해요!",
+      content: "축하해!",
+      createAt: new Date(),
+    },
+    {
+      RoomId: "567",
+      id: "Foden47",
+      content: "응원해줘서 고마워요. 다음주에 맛있는거 살게요!",
+      createAt: new Date(),
+    },
+    {
+      RoomId: "567",
+      id: "taewon2659",
+      content: "오 좋아! 다음주에 보자",
       createAt: new Date(),
     },
   ];
+
+  const MoveToProfile = () => {
+    router.push(`/${userInfo.id}`);
+  };
 
   return (
     <main className={style.main}>
@@ -55,7 +77,7 @@ const ChatRoom = () => {
       </div>
       <div className={style.articleContainer}>
         <div className={style.messagesBox}>
-          <div className={style.userInfoZone}>
+          <div className={style.userInfoZone} onClick={MoveToProfile}>
             <div className={style.userImage}>
               <img src={userInfo.img} className={style.image} />
             </div>
@@ -69,7 +91,7 @@ const ChatRoom = () => {
             </div>
             <div className={style.joinDate}>
               <p className={style.joinDateText}>
-                Joined {dayjs(userInfo.Joined).fromNow()}
+                Joined {dayjs(userInfo.Joined).format("MMMM YYYY")}
               </p>
             </div>
             <div className={style.followIntersection}>
@@ -78,7 +100,25 @@ const ChatRoom = () => {
               </p>
             </div>
           </div>
-          <div className={style.messageZone}>{messages.at(-1)?.content}</div>
+          <div className={style.messageZone}>
+            {messages.map((m) => {
+              return m.id === "taewon2659" ? (
+                <div className={style.myMessage}>
+                  <div className={style.content}>{m.content}</div>
+                  <div className={style.date}>
+                    {dayjs(m.createAt).format("MMM DD h:mm A")}
+                  </div>
+                </div>
+              ) : (
+                <div className={style.yourMessage}>
+                  <div className={style.content}>{m.content}</div>
+                  <div className={style.date}>
+                    {dayjs(m.createAt).format("MMM DD h:mm A")}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
         <div className={style.newMessageBox}></div>
       </div>
